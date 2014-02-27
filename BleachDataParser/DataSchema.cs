6d5437cef04e122479fa31613e386d24824c27be
bohua <fantasybohua@gmail.com>
2014-02-27 15:41:10 +0100

    startup

---
+++ BleachDataParser/DataSchema.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xlsParser
+{
+	class DailyReport
+	{
+		public DateTime report_date { get; set; }
+
+		public List<HourlyRecord> hourly_records { get; set; }
+
+		//日水量统计
+		//进水量
+		public decimal daily_sum_inbound_throughput_1 { get; set; }
+		public decimal daily_sum_inbound_throughput_2 { get; set; }
+		public decimal daily_sum_inbound_throughput_3 { get; set; }
+		public decimal daily_sum_inbound_total { get; set; }
+		//供水量
+		public decimal daily_sum_outbound_throughput_1 { get; set; }
+		public decimal daily_sum_outbound_throughput_2 { get; set; }
+		public decimal daily_sum_outbound_throughput_oldcityregion { get; set; }
+		public decimal daily_sum_outbound_throughput_southregion { get; set; }
+		public decimal daily_sum_outbound_throughput_total { get; set; }
+
+		//日加药量统计
+		//氯
+		public decimal daily_sum_consumption_cl_1 { get; set; }
+		public decimal daily_sum_consumption_cl_2 { get; set; }
+		public decimal daily_sum_consumption_cl_total { get; set; }
+		public decimal daily_sum_consumption_cl_avg { get; set; }
+		//矾
+		public decimal daily_sum_consumption_alun_1 { get; set; }
+		public decimal daily_sum_consumption_alun_2 { get; set; }
+		public decimal daily_sum_consumption_alun_3 { get; set; }
+		public decimal daily_sum_consumption_alun_total { get; set; }
+		public decimal daily_sum_consumption_alun_avg { get; set; }
+		//碱
+		public decimal daily_sum_consumption_alkali_1 { get; set; }
+		public decimal daily_sum_consumption_alkali_2 { get; set; }
+		public decimal daily_sum_consumption_alkali_3 { get; set; }
+		public decimal daily_sum_consumption_alkali_total { get; set; }
+		public decimal daily_sum_consumption_alkali_avg { get; set; }
+
+		//水质合格率
+		//沉淀出水
+		public decimal daily_sum_water_qulity_sediment_NTU_1 { get; set; }
+		public decimal daily_sum_water_qulity_sediment_NTU_2 { get; set; }
+		//滤后水
+		public decimal daily_sum_water_qulity_filtered_NTU { get; set; }
+		public decimal daily_sum_water_qulity_filtered_PH { get; set; }
+		public decimal daily_sum_water_qulity_filtered_leftCl { get; set; }
+		//出厂水
+		public decimal daily_sum_water_qulity_outbound_NTU { get; set; }
+		public decimal daily_sum_water_qulity_outbound_PH { get; set; }
+		public decimal daily_sum_water_qulity_outbound_leftCl { get; set; }
+		public decimal daily_sum_water_qulity_outbound_MPA { get; set; }
+
+		//日电量统计
+		//高配
+		//总有功
+		public decimal daily_sum_electron_highprofile_within_mark1 { get; set; }
+		public decimal daily_sum_electron_highprofile_within_mark2 { get; set; }
+		public decimal daily_sum_electron_highprofile_within_marktotal { get; set; }
+		//总无功
+		public decimal daily_sum_electron_highprofile_without_mark1 { get; set; }
+		public decimal daily_sum_electron_highprofile_without_mark2 { get; set; }
+		public decimal daily_sum_electron_highprofile_without_marktotal { get; set; }
+		//低配
+		//总有功
+		public decimal daily_sum_electron_lowprofile_within_mark1 { get; set; }
+		public decimal daily_sum_electron_lowprofile_within_mark2 { get; set; }
+		public decimal daily_sum_electron_lowprofile_within_marktotal { get; set; }
+		//总无功
+		public decimal daily_sum_electron_lowprofile_without_mark1 { get; set; }
+		public decimal daily_sum_electron_lowprofile_without_mark2 { get; set; }
+		public decimal daily_sum_electron_lowprofile_without_marktotal { get; set; }
+		//反冲洗房
+		public decimal daily_sum_electron_lowprofile_washroom_mark1 { get; set; }
+		public decimal daily_sum_electron_lowprofile_washroom_mark2 { get; set; }
+		public decimal daily_sum_electron_lowprofile_washroom_marktotal { get; set; }
+
+		//值班人员
+		//夜班
+		public string guard_night { get; set; }
+		//白班
+		public string guard_morning { get; set; }
+		//中班
+		public string guard_afternoon { get; set; }
+	}
+
+	class HourlyRecord
+	{
+		public DateTime record_time { get; set; }
+
+		//原水
+		public decimal inbound_NTU { get; set; }
+		public decimal inbound_PH { get; set; }
+		public decimal inbound_throughput_1 { get; set; }
+		public decimal inbound_throughput_2 { get; set; }
+		public decimal inbound_throughput_3 { get; set; }
+
+		//沉淀出水
+		public decimal sediment_NTU_1 { get; set; }
+		public decimal sediment_NTU_2 { get; set; }
+
+		//滤后水
+		public decimal filtered_NTU { get; set; }
+		public decimal filtered_PH { get; set; }
+		public decimal filtered_leftCl { get; set; }
+
+		//清水池
+		public decimal cleared_leftCl { get; set; }
+
+		//出厂水
+		public decimal outbound_NTU { get; set; }
+		public decimal outbound_PH { get; set; }
+		public decimal outbound_leftCl { get; set; }
+		public decimal outbound_MPA { get; set; }
+		public decimal outbound_throughput_1 { get; set; }
+		public decimal outbound_throughput_2 { get; set; }
+		public decimal outbound_throughput_oldtownregion { get; set; }
+		public decimal outbound_throughput_southregion { get; set; }
+		public decimal outbound_total { get; set; }
+
+		//机组总用电量
+		public decimal power_consumption { get; set; }
+
+		//配水单耗
+		public decimal manufactory_consumption { get; set; }
+	}
+}
--- BleachDataParser/Program.cs
+++ BleachDataParser/Program.cs
@@ -1,14 +1,192 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System.IO;
+using Newtonsoft.Json;
+using System.Reflection;
 
-namespace BleachDataParser
+namespace xlsParser
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			FileStream fileStream = new FileStream(@"Templates\report-daily.xls", FileMode.Open);
+			IWorkbook myWorkbook = new HSSFWorkbook(fileStream);
+
+			ISheet sheet = myWorkbook.GetSheetAt(2);
+
+			DailyReport report = new DailyReport();
+			report.hourly_records = new System.Collections.Generic.List<HourlyRecord>();
+
+			//获取报表日期
+			ICell dateCell = sheet.GetRow(1).GetCell(0);
+			try
+			{
+				report.report_date = Convert.ToDateTime(dateCell.ToString());
+			}
+			catch (Exception ex)
+			{
+				report.report_date = new DateTime();
+			}
+
+			//获取日计数
+			report.daily_sum_inbound_throughput_1 = injectIntoClass(sheet.GetRow(29).GetCell(4));
+			report.daily_sum_inbound_throughput_2 = injectIntoClass(sheet.GetRow(30).GetCell(4));
+			report.daily_sum_inbound_throughput_3 = injectIntoClass(sheet.GetRow(31).GetCell(4));
+			report.daily_sum_inbound_total = injectIntoClass(sheet.GetRow(32).GetCell(4));
+
+			report.daily_sum_outbound_throughput_1 = injectIntoClass(sheet.GetRow(33).GetCell(4));
+			report.daily_sum_outbound_throughput_2 = injectIntoClass(sheet.GetRow(34).GetCell(4));
+			report.daily_sum_outbound_throughput_oldcityregion = injectIntoClass(sheet.GetRow(35).GetCell(4));
+			report.daily_sum_outbound_throughput_southregion = injectIntoClass(sheet.GetRow(36).GetCell(4));
+			report.daily_sum_outbound_throughput_total = injectIntoClass(sheet.GetRow(37).GetCell(4));
+
+			report.daily_sum_consumption_cl_1 = injectIntoClass(sheet.GetRow(30).GetCell(10));
+			report.daily_sum_consumption_cl_2 = injectIntoClass(sheet.GetRow(31).GetCell(10));
+			report.daily_sum_consumption_cl_total = injectIntoClass(sheet.GetRow(30).GetCell(12));
+			report.daily_sum_consumption_cl_avg = injectIntoClass(sheet.GetRow(30).GetCell(13));
+
+			report.daily_sum_consumption_alun_1 = injectIntoClass(sheet.GetRow(32).GetCell(10));
+			report.daily_sum_consumption_alun_2 = injectIntoClass(sheet.GetRow(33).GetCell(10));
+			report.daily_sum_consumption_alun_3 = injectIntoClass(sheet.GetRow(34).GetCell(10));
+			report.daily_sum_consumption_alun_total = injectIntoClass(sheet.GetRow(32).GetCell(2));
+			report.daily_sum_consumption_alun_avg = injectIntoClass(sheet.GetRow(32).GetCell(13));
+
+			report.daily_sum_consumption_alkali_1 = injectIntoClass(sheet.GetRow(35).GetCell(10));
+			report.daily_sum_consumption_alkali_2 = injectIntoClass(sheet.GetRow(36).GetCell(10));
+			report.daily_sum_consumption_alkali_3 = injectIntoClass(sheet.GetRow(37).GetCell(10));
+			report.daily_sum_consumption_alkali_total = injectIntoClass(sheet.GetRow(35).GetCell(12));
+			report.daily_sum_consumption_alkali_avg = injectIntoClass(sheet.GetRow(35).GetCell(13));
+
+			report.daily_sum_water_qulity_sediment_NTU_1 = injectIntoClass(sheet.GetRow(29).GetCell(19));
+			report.daily_sum_water_qulity_sediment_NTU_2 = injectIntoClass(sheet.GetRow(30).GetCell(19));
+			report.daily_sum_water_qulity_filtered_NTU = injectIntoClass(sheet.GetRow(31).GetCell(19));
+			report.daily_sum_water_qulity_filtered_PH = injectIntoClass(sheet.GetRow(32).GetCell(19));
+			report.daily_sum_water_qulity_filtered_leftCl = injectIntoClass(sheet.GetRow(33).GetCell(19));
+			report.daily_sum_water_qulity_outbound_NTU = injectIntoClass(sheet.GetRow(34).GetCell(19));
+			report.daily_sum_water_qulity_outbound_PH = injectIntoClass(sheet.GetRow(35).GetCell(19));
+			report.daily_sum_water_qulity_outbound_leftCl = injectIntoClass(sheet.GetRow(36).GetCell(19));
+			report.daily_sum_water_qulity_outbound_MPA = injectIntoClass(sheet.GetRow(37).GetCell(19));
+
+			report.daily_sum_electron_highprofile_within_mark1 = injectIntoClass(sheet.GetRow(30).GetCell(23));
+			report.daily_sum_electron_highprofile_within_mark2 = injectIntoClass(sheet.GetRow(30).GetCell(24));
+			report.daily_sum_electron_highprofile_within_marktotal = injectIntoClass(sheet.GetRow(30).GetCell(25));
+
+			report.daily_sum_electron_highprofile_without_mark1 = injectIntoClass(sheet.GetRow(31).GetCell(23));
+			report.daily_sum_electron_highprofile_without_mark2 = injectIntoClass(sheet.GetRow(31).GetCell(24));
+			report.daily_sum_electron_highprofile_without_marktotal = injectIntoClass(sheet.GetRow(31).GetCell(25));
+
+			report.daily_sum_electron_lowprofile_within_mark1 = injectIntoClass(sheet.GetRow(32).GetCell(23));
+			report.daily_sum_electron_lowprofile_within_mark2 = injectIntoClass(sheet.GetRow(32).GetCell(24));
+			report.daily_sum_electron_lowprofile_within_marktotal = injectIntoClass(sheet.GetRow(32).GetCell(25));
+
+			report.daily_sum_electron_lowprofile_without_mark1 = injectIntoClass(sheet.GetRow(33).GetCell(23));
+			report.daily_sum_electron_lowprofile_without_mark2 = injectIntoClass(sheet.GetRow(33).GetCell(24));
+			report.daily_sum_electron_lowprofile_without_marktotal = injectIntoClass(sheet.GetRow(33).GetCell(25));
+
+			report.daily_sum_electron_lowprofile_washroom_mark1 = injectIntoClass(sheet.GetRow(34).GetCell(23));
+			report.daily_sum_electron_lowprofile_washroom_mark2 = injectIntoClass(sheet.GetRow(34).GetCell(24));
+			report.daily_sum_electron_lowprofile_washroom_marktotal = injectIntoClass(sheet.GetRow(34).GetCell(25));
+
+			report.guard_night = injectStringIntoClass(sheet.GetRow(35).GetCell(20));
+			report.guard_morning = injectStringIntoClass(sheet.GetRow(36).GetCell(20));
+			report.guard_afternoon = injectStringIntoClass(sheet.GetRow(37).GetCell(20));
+
+			//获取小时级计数
+			for (int i = 5; i < 29; i++)
+			{
+				HourlyRecord newEntry = new HourlyRecord();
+				IRow row = sheet.GetRow(i);
+
+				newEntry.record_time = injectTimeIntoClass(row.GetCell(0), report.report_date);
+
+				newEntry.inbound_NTU = injectIntoClass(row.GetCell(1));
+				newEntry.inbound_PH = injectIntoClass(row.GetCell(2));
+				newEntry.inbound_throughput_1 = injectIntoClass(row.GetCell(3));
+				newEntry.inbound_throughput_2 = injectIntoClass(row.GetCell(4));
+				newEntry.inbound_throughput_3 = injectIntoClass(row.GetCell(5));
+
+				newEntry.sediment_NTU_1 = injectIntoClass(row.GetCell(7));
+				newEntry.sediment_NTU_2 = injectIntoClass(row.GetCell(8));
+
+				newEntry.filtered_NTU = injectIntoClass(row.GetCell(9));
+				newEntry.filtered_PH = injectIntoClass(row.GetCell(10));
+				newEntry.filtered_leftCl = injectIntoClass(row.GetCell(11));
+
+				newEntry.cleared_leftCl = injectIntoClass(row.GetCell(12));
+
+				newEntry.outbound_NTU = injectIntoClass(row.GetCell(13));
+				newEntry.outbound_PH = injectIntoClass(row.GetCell(14));
+				newEntry.outbound_leftCl = injectIntoClass(row.GetCell(15));
+				newEntry.outbound_MPA = injectIntoClass(row.GetCell(16));
+				newEntry.outbound_throughput_1 = injectIntoClass(row.GetCell(17));
+				newEntry.outbound_throughput_2 = injectIntoClass(row.GetCell(19));
+				newEntry.outbound_throughput_oldtownregion = injectIntoClass(row.GetCell(20));
+				newEntry.outbound_throughput_southregion = injectIntoClass(row.GetCell(22));
+				newEntry.outbound_total = injectIntoClass(row.GetCell(23));
+
+				newEntry.power_consumption = injectIntoClass(row.GetCell(24));
+				newEntry.manufactory_consumption = injectIntoClass(row.GetCell(25));
+
+				report.hourly_records.Add(newEntry);
+			}
+
+			string json = JsonConvert.SerializeObject(report);
+			Console.Write(json);
+			//Console.ReadLine();
+		}
+
+		static private decimal injectIntoClass(ICell cell)
+		{
+			if (cell == null)
+			{
+				return 0;
+			}
+
+			try
+			{
+				return decimal.Round(decimal.Parse(cell.ToString()), 2);
+			}
+			catch (Exception ex)
+			{
+				return 0;
+			}
+		}
+
+		static private DateTime injectTimeIntoClass(ICell cell, DateTime dt)
+		{
+			if (cell == null)
+			{
+				return new DateTime();
+			}
+
+			try
+			{
+				int hour = int.Parse(cell.ToString());
+				return dt.Date.Add(new TimeSpan(hour, 0, 0));
+			}
+			catch (Exception ex)
+			{
+				return new DateTime();
+			}
+		}
+
+		static private string injectStringIntoClass(ICell cell)
+		{
+			if (cell == null)
+			{
+				return "";
+			}
+
+			try
+			{
+				return cell.ToString();
+			}
+			catch (Exception ex)
+			{
+				return "";
+			}
 		}
 	}
 }
